e117eb5712600d3da780694286220a163cc32c1f
Jayjay <gvcayetano@yahoo.com>
2013-05-27 06:42:06 +0800

    Added Localization keys

--- GuildWars2Events.Model/CouchbaseKeys.cs
+++ GuildWars2Events.Model/CouchbaseKeys.cs
@@ -10,7 +10,13 @@ namespace GuildWars2Events.Model
     {
         public const string ActiveWorlds = "ActiveWorlds";
         public const string EventNames = "EventNames";
+        public const string EventNamesFr = "EventNames_FR";
+        public const string EventNamesDe = "EventNames_DE";
+        public const string EventNamesEs = "EventNames_ES";
         public const string MapNames = "MapNames";
+        public const string MapNamesFr = "MapNames_FR";
+        public const string MapNamesDe = "MapNames_DE";
+        public const string MapNamesEs = "MapNames_ES";
         public const string ServiceState = "ServiceState";
         public const string Worlds = "Worlds";
 
