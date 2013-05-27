c62ebbfeeb0a314d1f826b7d0d6648583830d237
Jayjay <gvcayetano@yahoo.com>
2013-05-28 06:51:12 +0800

    Added Worlds Api
    Renamed Controllers to Controller
    Added the CouchbaseServer appsetting to web.config

--- GuildWars2Events.Model/World.cs
+++ GuildWars2Events.Model/World.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace GuildWars2Events.Model
 {
-    [Serializable]
+    [DataContract]
     public class World
     {
+        [DataMember]
         public int id { get; set; }
+        [DataMember]
         public string name { get; set; }
+        [DataMember]
         public DateTime LastActive { get; set; }
     }
 }
